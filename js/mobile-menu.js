1128664fc9627dd21157f7be3cb9bcd76f967c78
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-05-27 18:44:54 -0300

    mudei menu mobile

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,20 +3,21 @@ function menuMobile() {
   const navList = document.querySelector(".nav-list");
   const navLinks = document.querySelectorAll(".nav-list li");
 
-  function animateLinks() {
-    navLinks.forEach((link, index) => {
-      link.style.animation
-        ? (link.style.animation = "")
-        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-            index / 7 + 0.3
-          }s`);
+  function closeNav() {
+    function removeActive() {
+      navList.classList.remove("active");
+      mobileMenu.classList.remove("active");
+    }
+
+    navLinks.forEach((link) => {
+      link.addEventListener("click", removeActive);
     });
   }
 
   function handleClick() {
     navList.classList.toggle("active");
     mobileMenu.classList.toggle("active");
-    animateLinks();
+    closeNav();
   }
 
   mobileMenu.addEventListener("click", handleClick);
